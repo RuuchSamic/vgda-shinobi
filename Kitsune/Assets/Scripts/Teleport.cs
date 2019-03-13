81e4494b257bd9a5cef4523aca475a4568ebb841
Kenny Nguyen <baterra602@gmail.com>
2019-03-12 21:19:41 -0700

    Improved teleporting script so that it does not go through walls. Included a player manager script that holds the player object so that the teleport can call statically the manager's script to change the player's postition. Note that you need to set player object's tag and its children's tag to "Player" to prevent the player from teleporting to itself (See teleport script for more details).

--- Kitsune/Assets/Scripts/Teleport.cs
+++ Kitsune/Assets/Scripts/Teleport.cs
@@ -7,10 +7,19 @@ public class Teleport : MonoBehaviour
     // Start is called before the first frame update
     
    
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    // NOTE: be sure to set the tag for the player game object and its children as "Player" tag
+    // This collision will also check if the shuriken collides with the player itself when teleporting
+    // and if you dont set the player's tag to "Player", the player will teleport to itself
+    //
+    private void OnCollisionEnter2D(Collision2D collision) // on collision teleport the player
     {
-        PlayerManager.setPlayerPosition(collision.transform) ;
-        
+        if (collision.gameObject.tag != "Player") // make sure object being collided is not player
+        {
+            PlayerManager.setPlayerPosition(this.transform); // calls the static function of player manager to change
+                                                             // the player's position into the position where it collides with
+        }
+
+
+
     }
 }
