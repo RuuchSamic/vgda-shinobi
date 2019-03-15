328a5b96fb093fd4cf29221bda720fee4429a4e3
Kenny Nguyen <baterra602@gmail.com>
2019-03-15 14:10:16 -0700

    Attempts at making shuriken selection effect for menu where there are shurikens around the buttons that the player is selecting. Shurikens are there, but it cant move nor activate the buttons 0_0

--- Kitsune/Assets/Scripts/MenuBehavior.cs
+++ Kitsune/Assets/Scripts/MenuBehavior.cs
@@ -7,13 +7,13 @@ using UnityEngine.SceneManagement;
 public class MenuBehavior : MonoBehaviour
 {
 
-    public void LoadScene(string SceneName)
+    public void LoadScene(string SceneName) // changes scene to another scene based on name of scene passed
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneName);
     }
 
-    public void ExitGame()
+    public void ExitGame() // closes the game
     {
         Time.timeScale = 1f;
         Application.Quit();
