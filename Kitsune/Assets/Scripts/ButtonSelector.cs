5f06d8b1946617c4c41b836352b051e64f768c2d
Kenny Nguyen <baterra602@gmail.com>
2019-04-11 15:43:04 -0700

    Made a script that lets you navigate through the buttons on menu screen with keyboard keys. Doesn't change the location of shuriken icon to the selected button for some reason.

---
+++ Kitsune/Assets/Scripts/ButtonSelector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonSelector : MonoBehaviour
+{
+    //public Button[] Buttons; 
+   // public int buttonIndex;
+    public Selectable CurrButton;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("down") || Input.GetKeyDown("s"))
+        {
+           Selectable temp = CurrButton.FindSelectableOnDown();
+            if (temp != null)
+            {
+                CurrButton = temp;
+                CurrButton.Select();
+            }
+            
+        }
+
+        if (Input.GetKeyDown("up") || Input.GetKeyDown("w"))
+        {
+            Selectable temp = CurrButton.FindSelectableOnUp();
+            if (temp != null)
+            {
+                CurrButton = temp;
+                CurrButton.Select();
+            }
+        }
+    }
+}
