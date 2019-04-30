a35b270afbf2d7eb11c9750c7be62db97c3a53c0
maddiepj <maddiepj@gmail.com>
2019-04-30 01:36:44 -0700

    Added enemy AI logic (mostly pseudocode) with TODO's to be implemented

--- Kitsune/Assets/Scripts/Enemy.cs
+++ Kitsune/Assets/Scripts/Enemy.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    //TO DO: add booleans for animation states, e.g. anim.setBool("isFollowing", true);?
     public static bool seesPlayer;
     private Animator anim;
     public float speed;
     public float distance;
-    public int state; // 0 = idle; 1 = atk; 2 = walk;
+    public int state; // 0 = idle; 1 = atk; 2 = walk; 3 = followPlayer;
 
     private bool movingRight = true;
     [SerializeField] private LayerMask m_CollideWith;
@@ -30,27 +32,46 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
+        // TO DO: add ray that only detects if player is in close enough range to attack; doesn't collide with anything
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance, m_CollideWith);
+        RaycastHit2D wallInfo = Physics2D.Raycast(groundDetection.position, Vector2.right, 0, m_CollideWith);
 
         // if idle
         if (state == 0)
         {
-
+            // while (seesPlayer && no ledge) go to follow state
+            if(seesPlayer && groundInfo.collider == true)
+            {
+                //set state to followPlayer
+                //move toward player
+                //when in atk range, change state to == 1
+                state = 3;
+            }
+            //if the player is not in sight of the enemy, have enemy patrol
+            else if (!seesPlayer)
+            {
+                state = 2;
+            }
         }
 
-        // if atk 
+        // TO DO: if atk 
         if (state == 1)
         {
-
+            //if player in attack range, attack player
+            // if player not in attack range, switch to follow if seesPlayer == true or patrol if !seesPlayer
         }
 
-        // if walking
+        // if patrol
         if (state == 2)
         {
+            // if the enemy sees the player while patrolling, change to followPlayer state
+            if (seesPlayer)
+            {
+                state = 3;
+            }
+
             transform.Translate(Vector2.right * speed * Time.deltaTime);
             
-            RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance, m_CollideWith);
-            RaycastHit2D wallInfo = Physics2D.Raycast(groundDetection.position, Vector2.right, 0, m_CollideWith);
-
             Debug.DrawRay(groundDetection.position, Vector2.right, Color.red, distance);
 
             if (groundInfo.collider == false || wallInfo.collider == true)
@@ -70,6 +91,20 @@ public class Enemy : MonoBehaviour
 
 
             }
+        }
+
+        // if follow player
+        if (state == 3)
+        {
+            //TO DO: get player's current transform.position and move toward it
+
+            //if enemy detects a ledge while player in sight, switch to idle state, rather than turn around and keep walking in opposite direction
+            if (seesPlayer && groundInfo.collider == false)
+            {
+                state = 0;
+            }
+
+            // TO DO: if player in attack range, switch state to attack
         }
    
     }
