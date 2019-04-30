2dfd0829978635e868a33b6a452ef385921e2eee
Kenny Nguyen <baterra602@gmail.com>
2019-04-29 17:07:03 -0700

    Made the ui tools on top right screen. Added scripts that enables the selection box(its green) to select the tool associated with the keys. Will be turning this into a prefab for use.

---
+++ Kitsune/Assets/Scripts/UITools.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITools : MonoBehaviour
+{
+
+    public GameObject tool1; // the selectors ontop of the associated tools
+    public GameObject tool2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1") == true) 
+        {
+            tool2.SetActive(false); // remove the selection on tool2
+            tool1.SetActive(true); // select tool 1
+
+        }
+
+        if (Input.GetKeyDown("2") == true)
+        {
+            tool1.SetActive(false);
+            tool2.SetActive(true);
+        }
+
+    }
+}
